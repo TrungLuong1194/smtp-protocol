2d2fee81fd58d70ceddf021cac4150609696861d
TrungLuong1194 <trungluong1194@gmail.com>
2021-01-02 22:52:05 +0300

    update

--- src/utility.h
+++ src/utility.h
@@ -4,14 +4,14 @@
 #include <stdio.h>
 #include <sys/socket.h>
 
+enum dataConstants {
+	BUFSIZE = 512,
+};
+
 // Handle error with user
 void errorUser(char *msg, char *detail);
 
 // Handle error with system
 void errorSystem(char *msg);
-
-enum dataConstants {
-	BUFSIZE = 512,
-};
 
 #endif
